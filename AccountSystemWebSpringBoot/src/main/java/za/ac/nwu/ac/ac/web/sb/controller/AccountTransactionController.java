400cc56f5564271934f43972744a4944e1d28705
shene <sheneboshoff6@gmail.com>
2021-10-06 16:33:15 +0200

    Fixed queries for AccountTransaction

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/ac/web/sb/controller/AccountTransactionController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/ac/web/sb/controller/AccountTransactionController.java
@@ -1,15 +1,14 @@
 package za.ac.nwu.ac.ac.web.sb.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import za.ac.nwu.ac.domain.dto.AccountTransactionDto;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
@@ -47,4 +46,19 @@ public class AccountTransactionController {
         GeneralResponse<List<AccountTransactionDto>> response = new GeneralResponse<>(true, accountTypes);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @PostMapping("")
+    @ApiOperation(value = "Creates a new AccountTransaction.", notes = "Creates a new AccountTransaction for the specified Member.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "The AccountType was created successfully", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+    public ResponseEntity<GeneralResponse<AccountTransactionDto>> create(
+            @ApiParam(value = "Request body to create a new AccountTransaction.", required = true)
+            @RequestBody AccountTransactionDto accountTransaction){
+        AccountTransactionDto accountTransactionResponse = createAccountTransactionFlow.create(accountTransaction);
+        GeneralResponse<AccountTransactionDto> response = new GeneralResponse<>(true, accountTransactionResponse);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
